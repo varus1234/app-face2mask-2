5a15f22c2835e1957c65750eb8bc15b2b86b625e
varus1234 <varus.46824.varus@gmail.com>
2020-12-09 11:40:58 +0000

    modify app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from preprocess import return_face
 
 
 st.title('face2mask')
+st.text('※画像が表示されない場合は、右上のメニューからRerunしてください。')
 
 uploaded_file = st.file_uploader('マスクを付与したい画像を選択してください。', type=['jpg','png','jpeg'])
 if uploaded_file is not None:
